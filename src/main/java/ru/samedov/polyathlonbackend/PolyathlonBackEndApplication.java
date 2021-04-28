769057e61a773c87d2ad0d8e8c83fea960c0bca3
Aleksandr Samedov <samedov@advantum.pro>
2021-04-28 15:13:06 +0300

    Initial commit

---
+++ src/main/java/ru/samedov/polyathlonbackend/PolyathlonBackEndApplication.java
@@ -0,0 +1,13 @@
+package ru.samedov.polyathlonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyathlonBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyathlonBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/samedov/polyathlonbackend/PolyathlonBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.samedov.polyathlonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyathlonBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
